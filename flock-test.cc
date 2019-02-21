0d29086d27d7b6955251f92e0a58b4a413395229
Lizhou Gao <lizhou.gao@ucloud.cn>
2019-02-21 15:42:18 +0800

    update

--- flock-test.cc
+++ flock-test.cc
@@ -5,6 +5,28 @@
 #include <fcntl.h>
 #include <sys/file.h>
 #include <glog/logging.h>
+#include <sys/mman.h>
+
+namespace {
+int resize_and_map(int fd, void *addr, size_t len)
+{
+  void *map_addr;
+
+  if (ftruncate(fd, len)) {
+    LOG(ERROR) << "Cannot truncate";
+    return -1;
+  }
+
+  map_addr = mmap(addr, len, PROT_READ | PROT_WRITE,
+                  MAP_SHARED | MAP_FIXED, fd, 0);
+  if (map_addr != addr) {
+    LOG(ERROR) << "mmap() failed: " << strerror(errno);
+    return -1;
+  }
+  return 0;
+}
+
+}
 
 void flock_test() {
   char *lock_file = "flock-test.lock";
@@ -23,12 +45,10 @@ void flock_test() {
     close(fd);
     return;
   }
-  int fdlist[4096];
-  for (int i=0;i<4096;i++) {
-    //fdlist[i] = open(lock_file, O_CREAT | O_RDWR, 0600);
-  }
+  char data[100];
+  resize_and_map(fd, data, 100);
+  close(fd);
   LOG(INFO) << "Sucessfully in flock " << lock_file << ", now sleep 10 seconds";
   sleep(10);
   LOG(INFO) << "Now Finish";
-  close(fd);
 }
